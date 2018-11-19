e0b2decd1d98629befa7bd25ce6eaaab9d48f6b2
Nickolas <nickolasfrederik@gmail.com>
2018-11-19 16:43:05 -0200

    [add] ant design

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Button from 'antd/lib/button';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            TODO
-          </p>
-        </header>
+        <Button type="primary">Button</Button>
       </div>
     );
   }
