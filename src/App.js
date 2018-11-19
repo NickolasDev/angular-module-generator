5cc74cfc5675657303ff324053b0385c79b9ae1d
Nickolas <nickolasfrederik@gmail.com>
2018-11-19 17:43:07 -0200

    [add] Basic router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import Button from 'antd/lib/button';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from "./containers/Home";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Button type="primary">Button</Button>
-      </div>
+      <Router>
+          <Route path="/" exact component={Home} />
+      </Router>
     );
   }
 }
